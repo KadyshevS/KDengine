0827e0e701bd149656117702b5230e2444e99927
KadyshevS <kadyshev74@gmail.com>
2022-11-10 08:30:44 +0500

    Fixed attenuate mul

--- KDengine/PhongPS.hlsl
+++ KDengine/PhongPS.hlsl
@@ -18,19 +18,19 @@ cbuffer ObjectCBuf
 
 float4 main(float3 worldPos : Position, float3 n : Normal) : SV_Target
 {
-	// fragment to light vector data
+	// Fragment to light vector data
 	const float3 vToL = lightPos - worldPos;
 	const float distToL = length(vToL);
 	const float3 dirToL = vToL / distToL;
-	// diffuse attenuation
+	// Attenuation
 	const float att = 1.0f / ( attConst + attLin * distToL + attQuad * (distToL * distToL) );
-	// diffuse intensity
+	// Diffuse intensity
 	const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f,dot(dirToL,n));
-	// reflected light vector
+	// Reflected light vector
 	const float3 w = n * dot( vToL, n );
 	const float3 r = w * 2.0f - vToL;
-	// calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
-	const float3 specular = (diffuseColor * diffuseIntensity) * specularIntensity * pow( max( 0.0f, dot( normalize(-r), normalize(worldPos) ) ), specularPower );
-	// final color
+	// Calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
+	const float3 specular = att * (diffuseColor * diffuseIntensity) * specularIntensity * pow( max( 0.0f, dot( normalize(-r), normalize(worldPos) ) ), specularPower );
+	// Final color
 	return float4(saturate((diffuse + ambient + specular) * materialColor),1.0f);
 }
