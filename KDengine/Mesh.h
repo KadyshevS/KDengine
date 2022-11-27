b0985e75291b6d941ecc6b9bfcc19400d4aa243a
KadyshevS <kadyshev74@gmail.com>
2022-11-27 11:47:04 +0500

    Removing constant path to model / Reorganized shader code files

--- KDengine/Mesh.h
+++ KDengine/Mesh.h
@@ -7,6 +7,7 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 #include "ConstantBuffer.h"
+#include <filesystem>
 #include <type_traits>
 #include "imgui.h"
 
@@ -128,13 +129,13 @@ private:
 class Model
 {
 public:
-	Model( Graphics& gfx,const std::string fileName );
+	Model( Graphics& gfx,const std::string& pathString );
 	void Draw( Graphics& gfx ) const noexcept(!IS_DEBUG);
 	void ShowWindow( Graphics& gfx, const char* windowName = nullptr ) noexcept;
 	void SetRootTransform( DirectX::FXMMATRIX tf ) noexcept;
 	~Model() noexcept;
 private:
-	static std::unique_ptr<Mesh> ParseMesh( Graphics& gfx,const aiMesh& mesh, const aiMaterial* const* pMaterials );
+	static std::unique_ptr<Mesh> ParseMesh( Graphics& gfx,const aiMesh& mesh, const aiMaterial* const* pMaterials,const std::filesystem::path& path );
 	std::unique_ptr<Node> ParseNode( int& nextId,const aiNode& node ) noexcept;
 private:
 	std::unique_ptr<Node> pRoot;
